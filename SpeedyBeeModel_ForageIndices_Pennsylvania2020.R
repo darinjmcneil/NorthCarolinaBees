d124477c6e228ebedda7e40ff9f866c11747d1c6
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-03-09 18:49:50 -0500

    update forage table to add new CDL 2020 class and re-run model with updated forage info.

--- SpeedyBeeModel_ForageIndices_Pennsylvania2020.R
+++ SpeedyBeeModel_ForageIndices_Pennsylvania2020.R
@@ -5,20 +5,23 @@
 
 # install.packages("devtools")
 # install.packages("raster")
-# library(devtools)
-# devtools::install_github("land-4-bees/SpeedyBeeModel")
+library(devtools)
+devtools::install_github("land-4-bees/SpeedyBeeModel")
 
 library(sf); library(raster); library(logger); library(tidyr); library(SpeedyBeeModel)
-
+rm(list=ls())
 # bring in cropland data layer (CDL)
 
 cdl_2020 <- raster("E:\\2021_NC_Bees\\NorthCarolinaBees2021\\2020_30m_cdls\\2020_30m_cdls.img") # read in CDL
+#cdl_2020 <- raster::raster('Y:/SpatialData/NASS_CDL/CDL2020/2020_30m_cdls.img') #file path on Melanie's computer
 plot(cdl_2020, main="CDL 2020") # map CDL
 
-# bring in shapefile of states
+# bring in shapefile of states (use an R package to grab this shapefile to avoid having to change the file path)
+us_states1 <- tigris::states() %>% 
+  sf::st_transform(us_states1, crs = crs(cdl_2020))
 
-us_states1 <- st_read("E:\\FireflyAnalysis_October2020\\states\\us_states.shp") #  read shapefile of US states
-us_states1 <- sf::st_transform(us_states1, crs = crs(cdl_2020)) # reproject to match CDL
+# us_states1 <- st_read("E:\\FireflyAnalysis_October2020\\states\\us_states.shp") #  read shapefile of US states
+# us_states1 <- sf::st_transform(us_states1, crs = crs(cdl_2020)) # reproject to match CDL
 
 # overlay maps
 
@@ -26,15 +29,15 @@ plot(us_states1$geometry, add = TRUE) # they look good!
 
 # extract Pennsylvania polygon
 
-PennsylvaniaBoundary <- subset(us_states1, name == "Pennsylvania") # extract state boundary polygon
-plot(Pennsylvania$geometry) # nice!
+PennsylvaniaBoundary <- subset(us_states1, NAME == "Pennsylvania") # extract state boundary polygon
+plot(PennsylvaniaBoundary$geometry) # nice!
 
-# use NC polygon to extract CDL for PA
+# use polygon to extract CDL for PA
 
-CroppedCDL <- crop(cdl_2020, PennsylvaniaBoundary) # crop CDL using PA boundary polygon
+CroppedCDL <- raster::crop(cdl_2020, PennsylvaniaBoundary) # crop CDL using PA boundary polygon
 MaskedCDL <- raster::mask(CroppedCDL, PennsylvaniaBoundary) # mask cropped CDL using PA bounds
 plot(MaskedCDL, main="CDL, North Carolina 2016") # this takes a minute or two
-writeRaster(MaskedCDL, "E:\\2021_NC_Bees\\NorthCarolinaBees2021\\PA_CDL2020_Raster.tif", overwrite = TRUE) # takes a minute or two
+writeRaster(MaskedCDL, "./PA_CDL2020_Raster.tif", overwrite = TRUE) # takes a minute or two
 # unique(MaskedCDL)
 # dev.off()
 # hist(MaskedCDL)
@@ -68,14 +71,14 @@ plot(PA2020map) # hell yea it did!
 
 ##################################### [LONSDORF INDICES]
 
-ForageTable1 <- read.csv("E:\\2021_NC_Bees\\NorthCarolinaBees2021\\cdl_reclass_forage_quality.csv")
+ForageTable1 <- read.csv("./cdl_reclass_forage_quality_updated.csv")
 # View(ForageTable1)
 
 SpeedyBeeModel::forage_index( 
-    output_dir = "E:\\2021_NC_Bees\\NorthCarolinaBees2021\\PA2020_LonsdorfIndices", # output loc
-    landcover_path  = "E:\\2021_NC_Bees\\NorthCarolinaBees2021\\PA_CDL2020_Raster.tif", # path to NC CDL map
+    output_dir = "./PA2020_LonsdorfIndices", # output loc
+    landcover_path  = "./PA_CDL2020_Raster.tif", # path to PA CDL map
     forage_table = ForageTable1,
-    seasons = c("Floral_Spring"),
+    seasons = c("Floral_Spring", "Nest_Cavity"),
     forage_range = 2000,
     guild_table = NA,
     agg_factor = NA,
@@ -83,10 +86,10 @@ SpeedyBeeModel::forage_index(
     compress_rasters = T
 )
 
-PAfloral <- raster("E:\\2021_NC_Bees\\NorthCarolinaBees2021\\PA2020_LonsdorfIndices\\PA_CDL2020_Raster_Floral_Spring.tif")
+PAfloral <- raster("./PA2020_LonsdorfIndices/PA_CDL2020_Raster_Floral_Spring.tif")
 plot(PAfloral)
 
-PAnest <- raster("E:\\2021_NC_Bees\\NorthCarolinaBees2021\\PA2020_LonsdorfIndices\\PA_CDL2020_Raster_Nest_Cavity.tif")
+PAnest <- raster("./PA2020_LonsdorfIndices/PA_CDL2020_Raster_Nest_Cavity.tif")
 plot(PAnest)
 
 # getting an error about missing raster classes
